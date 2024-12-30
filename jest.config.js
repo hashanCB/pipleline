141fc4d02b4a30757776d8190ab101e838768a4d
chanaka <hashan.chanaka96@gmail.com>
2024-12-30 20:14:14 +0000

    build

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  testMatch: [
+    "<rootDir>/app/**/*.test.js", // Adjust the path based on where your test files are
+    "<rootDir>/app/**/*.spec.js",
+  ],
+  moduleFileExtensions: ["js", "jsx", "json", "node"],
+};
